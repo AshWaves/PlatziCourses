19c7f2d73e20eb88364607ca2ae24b4a91a1c9f5
AshWavesCode <ashwavescode@gmail.com>
2022-09-25 17:52:12 -0500

    web of mokepon

---
+++ CursoDeProgramacionBasica/Mokepon/js/mokepon.js
@@ -0,0 +1,6 @@
+function actionPetButton(){
+    alert("Has seleccionado tu mascota")
+}
+
+let selecPetButton=document.getElementById("button-pet");
+selecPetButton.addEventListener("click",actionPetButton);
