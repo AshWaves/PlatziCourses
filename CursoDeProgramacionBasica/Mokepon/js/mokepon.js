d4d16dfcd5b57673853053b58ff7903c88501bb8
AshWavesCode <ashwavescode@gmail.com>
2022-09-25 19:04:14 -0500

    Listener Mokepon

--- CursoDeProgramacionBasica/Mokepon/js/mokepon.js
+++ CursoDeProgramacionBasica/Mokepon/js/mokepon.js
@@ -1,6 +1,19 @@
-function actionPetButton(){
-    alert("Has seleccionado tu mascota")
+function actionPetButton(){// creo la funcion
+    if(document.getElementById("Hipodoge")){// con el getElementById capturo un id de mi documento html y lo comparo, si es asi funcionara alguna de las condicionales
+        alert("Has seleccionado a hipoge")
+    }else if (document.getElementById("Capipepo")){
+        alert("Has seleccionado a Capipepo")
+    }else if(document.getElementById("Ratigueya")){
+        alert("Has seleccionado a Ratigueya")
+    }else if(document.getElementById("Langostelvis")){
+        alert("Has seleccionado a Langostelvis")
+    }else if(document.getElementById("Pydos")){
+        alert("Has seleccionado a Pydos")
+    }
 }
-
-let selecPetButton=document.getElementById("button-pet");
-selecPetButton.addEventListener("click",actionPetButton);
+function startGame(){
+    let selecPetButton=document.getElementById("button-pet");// capturo un id de mi documento html
+    selecPetButton.addEventListener("click",actionPetButton);//escuha el evento y crea la accion de ese evento
+}
+//añado el window para que escuche cuando mi pagina carga por completo y inicia la funcion star game
+window.addEventListener("load",startGame)
