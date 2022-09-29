a86a10b0d6865e061fe96221ed9a7a8dcfb2648f
AshWavesCode <ashwavescode@gmail.com>
2022-09-29 01:59:41 -0500

    function randomPet and petEnemi

--- CursoDeProgramacionBasica/Mokepon/js/mokepon.js
+++ CursoDeProgramacionBasica/Mokepon/js/mokepon.js
@@ -1,27 +1,48 @@
-function actionPetButton(){// creo la funcion
-    let inputHipodoge=document.getElementById("Hipodoge")
-    let inputCapipepo=document.getElementById("Capipepo")
-    let inputRatigueya=document.getElementById("Ratigueya")
-    let inputLangostelvis=document.getElementById("Langostelvis")
-    let inputPydos=document.getElementById("Pydos")
-    let spanMascotaPlayer=document.getElementById("Pet-Player")
-    if(inputHipodoge.checked){// con el getElementById capturo un id de mi documento html y lo comparo, si es asi funcionara alguna de las condicionales 
-        spanMascotaPlayer.innerHTML="Hipodoge"
-    }else if (inputCapipepo.checked){
-        spanMascotaPlayer.innerHTML="Capipepo"
-    }else if(inputRatigueya.checked){
-        spanMascotaPlayer.innerHTML="Ratigueya"
-    }else if(inputLangostelvis.checked){
-        spanMascotaPlayer.innerHTML="Langostelvis"
-    }else if(inputPydos.checked){
-        spanMascotaPlayer.innerHTML="Pydos"
+function selectPet(){
+    let petSelectionHipodoge=document.getElementById("Hipodoge")
+    let petSelectionCapipepo=document.getElementById("Capipepo")
+    let petSelectionRatigueya=document.getElementById("Ratigueya")
+    let petSelectionLangostelvis=document.getElementById("Langostelvis")
+    let petSelectionPydos=document.getElementById("Pydos")
+    let spanPlayerPet=document.getElementById("Pet-Player")
+    if(petSelectionHipodoge.checked){
+        spanPlayerPet.innerHTML="Hipodoge"
+    }else if(petSelectionCapipepo.checked){
+        spanPlayerPet.innerHTML="Capipepo"
+    }else if(petSelectionRatigueya.checked){
+        spanPlayerPet.innerHTML="Ratigueya"
+    }else if(petSelectionLangostelvis.checked){
+        spanPlayerPet.innerHTML="Langostelvis"
+    }else if(petSelectionPydos.checked){
+        spanPlayerPet.innerHTML="Pydos"
     }else{
-        alert("Debes seleccionar un Mokepon")
+        alert("Debes de seleccionar una mascota")
+    }
+    petEnemi()   
+}
+function petEnemi(){
+    let spanEnemi=document.getElementById("Pet-Enemi")
+    let selectPet=randomPet(1,3)
+    if(selectPet==1){
+        spanEnemi.innerHTML="Hipodoge"
+    }else if(selectPet==2){
+        spanEnemi.innerHTML="Capipepo"
+    }else if(selectPet==3){
+        spanEnemi.innerHTML="Ratigueya"
+    }else if(selectPet==4){
+        spanEnemi.innerHTML="Langostelvis"
+    }else if(selectPet==5){
+        spanEnemi.innerHTML="Pydos"
     }
 }
-function startGame(){
-    let selecPetButton=document.getElementById("button-pet");// capturo un id de mi documento html
-    selecPetButton.addEventListener("click",actionPetButton);//escuha el evento y crea la accion de ese evento
+function randomPet(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min)
 }
-//añado el window para que escuche cuando mi pagina carga por completo y inicia la funcion star game
-window.addEventListener("load",startGame)
+function starGame(){
+    let StarButton=document.getElementById("button-pet")
+    StarButton.addEventListener("click",selectPet)
+}
+
+window.addEventListener("load",starGame)
+
+        let spanEnemi=document.getElementById("Pet-Enemi")
