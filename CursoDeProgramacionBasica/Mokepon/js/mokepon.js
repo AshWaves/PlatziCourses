a5899a9ac815c5edea94ade6bb1a71e87aa18d65
AshWavesCode <ashwavescode@gmail.com>
2022-09-29 04:11:32 -0500

    Create a functions attack and change button for the checkbox

--- CursoDeProgramacionBasica/Mokepon/js/mokepon.js
+++ CursoDeProgramacionBasica/Mokepon/js/mokepon.js
@@ -1,3 +1,6 @@
+let attackPlayer
+let attackEnemi
+
 function selectPet(){
     let petSelectionHipodoge=document.getElementById("Hipodoge")
     let petSelectionCapipepo=document.getElementById("Capipepo")
@@ -22,9 +25,10 @@ function selectPet(){
 }
 function petEnemi(){
     let spanEnemi=document.getElementById("Pet-Enemi")
-    let selectPet=randomPet(1,3)
+    let selectPet=randomAttackAndPet(1,5)
     if(selectPet==1){
         spanEnemi.innerHTML="Hipodoge"
+        //attackEnemi="Water"
     }else if(selectPet==2){
         spanEnemi.innerHTML="Capipepo"
     }else if(selectPet==3){
@@ -34,15 +38,43 @@ function petEnemi(){
     }else if(selectPet==5){
         spanEnemi.innerHTML="Pydos"
     }
+
 }
-function randomPet(min,max){
+function randomAttackAndPet(min,max){
     return Math.floor(Math.random()*(max-min+1)+min)
+}
+
+function attackFire(){
+    attackPlayer="Fire"
+    alert(attackPlayer)
+}
+function attackWater(){
+    attackPlayer="Water"
+    alert(attackPlayer)
+}
+function attackEarth(){
+    attackPlayer="Earth"
+    alert(attackPlayer)
+}
+function CombinationAttackFireAndWater(){
+    attackFire
+    attackWater
+}
+function combinationAttackEarhtAndFire(){
+    attackEarth
+    attackFire
 }
 function starGame(){
     let StarButton=document.getElementById("button-pet")
     StarButton.addEventListener("click",selectPet)
+    let buttonFire=document.getElementById("button-fire")
+    let buttonWater=document.getElementById("button-water")
+    let buttonEarth=document.getElementById("button-earth")
+    buttonFire.addEventListener("click",attackFire)
+    buttonWater.addEventListener("click",attackWater)
+    buttonEarth.addEventListener("click",attackEarth)
+
 }
 
 window.addEventListener("load",starGame)
 
-        let spanEnemi=document.getElementById("Pet-Enemi")
